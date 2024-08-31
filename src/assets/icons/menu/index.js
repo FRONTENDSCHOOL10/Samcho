c0e215606d0fa745393c618d0c4251fe5ba6a709
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-01 00:44:46 +0900

    setting: 개발 관련 에셋 추가
    - 메뉴 아이콘에 쓰이는 svg 파일 추가
      - path의 fill 속성 current로 변경함.

---
+++ src/assets/icons/menu/index.js
@@ -0,0 +1,6 @@
+import Calendar2 from './calendar2.svg?react';
+import Filter from './filter.svg?react';
+import List from './list.svg?react';
+import Search from './search.svg?react';
+
+export { Calendar2, Filter, List, Search };
