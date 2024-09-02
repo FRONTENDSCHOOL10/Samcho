77ebe152f6239caba52262fe6954682d3ffb5ba7
paintover <tjdus1139@naver.com>
2024-09-02 11:00:15 +0900

    feat : EmotionDistribution 컴포넌트 마크업/스타일링 완료
    - mood : 감정 5개 중 하나를 props(string)로 받을 수 있도록 함 (bad, sad, happy, smile, soso)
    - ratio : 라벨의 감정 분포 비율을 props(number)로 받을 수 있도록 함
    - mood에 따라 alt를 다르게 받을 수 있도고 함
    
    resolves #20

---
+++ src/components/atomic/EmotionDistribution.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import { BadMong, SadMong, HappyMong, SmileMong, SosoMong } from '@/assets/icons/mood';
+
+const EmotionDistribution = ({mood = 'happy', ratio = 100}) => {
+  const moods = {
+    bad: BadMong,
+    sad: SadMong,
+    happy: HappyMong,
+    smile: SmileMong,
+    soso: SosoMong
+  }
+
+  let altText;
+
+  switch (mood) {
+    case 'happy':
+      altText = '행복한 감정';
+      break;
+    case 'sad':
+      altText = '슬픈 감정';
+      break;
+    case 'bad':
+      altText = '기분 나쁜 감정';
+      break;
+    case 'smile':
+      altText = '기분 좋은 감정';
+      break;
+    case 'soso':
+      altText = '그저 그런 감정';
+      break;
+    default:
+      altText = '행복한 감정';
+  }
+
+
+  const containerClasses = `w-[45px] h-[68px] flex flex-col gap-3 items-center`
+  const imgClasses = `w-[36px] h-[36px]`;
+  const ratioClasses = `flex justify-center items-center w-[45px] h-[20px] bg-blue-10 rounded-xl self-stretch text-xs font-medium text-blue-500 text-center`;
+
+
+  return (
+    <>
+    <div className={containerClasses}>
+      <img src={moods[mood]} alt={altText} className={imgClasses}></img>
+      <span className={ratioClasses}>{`${ratio}%`}</span>
+    </div>
+    </>
+  )
+
+}
+
+EmotionDistribution.propTypes = {
+  ratio: PropTypes.number,
+  mood: PropTypes.oneOf(['bad', 'sad', 'happy', 'smile', 'soso'])
+}
+
+export default EmotionDistribution;
