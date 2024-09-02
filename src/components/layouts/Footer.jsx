9b00c420ec7d0eacada69c9b8024d48e2539dcd6
ehh99 <jinung5@gmail.com>
2024-09-02 09:44:38 +0900

    feat: TextArea 컴포넌트 마크업 작업 및 GNB 내비게이션 작업 - TextArea 최대 200자 까지의 텍스트만 받을 수 있음 - TextArea 최대 6줄까지 작성을 할 수 있음. 200자에 맞게 최대 줄을 설정함. - 기본 높이값 50에서 최대 6줄의 스크롤까지 개행되는 만큼 자동으로 높이 증가 - GNB 내비게이션 작업 일부 완료 - 완성된 기타 컴포넌트들 조합 완료

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,16 +1,41 @@
 import { Calendar, Chart, Letter, Pencil, User } from '@/assets/icons/gnb';
-import DayCircle from '@/assets/icons/daycircle/daycircle.svg?react';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Button } from '../';
 
 const Footer = () => {
+  const currentUrl = useLocation().pathname;
+  console.log(currentUrl);
+  const getLinkClassName = (path) => {
+    return currentUrl === path ? 'fill-blue-500' : 'fill-gray-300';
+  };
+
+  if (currentUrl === '/diary/new') {
+    return (
+      <footer className="fixed bottom-0 w-full max-w-[27.5rem] bg-white py-4 z-50 shadow-top -mx-5 px-5">
+        <Button size="large">작성완료</Button>
+      </footer>
+    );
+  }
+
   return (
-    <footer className="fixed bottom-0 w-full max-w-[27.5rem] bg-white py-4 z-50 shadow-light">
+    <footer className="fixed bottom-0 w-full max-w-[27.5rem] bg-white py-4 z-50 shadow-top -mx-5">
       <nav className="flex items-center justify-around">
-        <Calendar className="cursor-pointer fill-blue-500" />
-        <Chart className="cursor-pointer fill-gray-300" />
-        <Letter className="cursor-pointer fill-gray-300" />
-        <Pencil className="cursor-pointer fill-gray-300" />
-        <User className="cursor-pointer fill-gray-300" />
-        <DayCircle className="fill-blue" />
+        <Link to={'/'}>
+          <Calendar className={`cursor-pointer ${getLinkClassName('/')}`} />
+        </Link>
+        <Link to={'/chart'}>
+          <Chart className={`cursor-pointer ${getLinkClassName('/chart')}`} />
+        </Link>
+        <Link to={'/diary/new'}>
+          <Pencil className={`cursor-pointer fill-gray-300`} />
+        </Link>
+        <Link to={'/post'}>
+          <Letter className={`cursor-pointer ${getLinkClassName('/post')}`} />
+        </Link>
+        <Link to={'/mypage'}>
+          <User className={`cursor-pointer ${getLinkClassName('/mypage')}`} />
+        </Link>
       </nav>
     </footer>
   );
