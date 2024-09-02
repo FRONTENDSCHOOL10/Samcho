dc98b7b82f9b8dfe206a50116c899fe3a65c83dc
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-02 10:35:21 +0900

    setting: 컴포넌트 index 파일로 export 시키기

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './atomic/Button.jsx';
+export { default as FeelingCalendar } from './atomic/FeelingCalendar.jsx';
 export { default as TopNavigation } from './layouts/TopNavigation.jsx';
 export { default as TextArea } from './atomic/TextArea.jsx';
 export { default as Footer } from './layouts/Footer.jsx';
