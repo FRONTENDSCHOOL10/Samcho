599c3240118b8c1bb709ab39c987cd24cec0413d
이영빈 <gmlyb@naver.com>
2024-09-02 09:51:03 +0900

    feat: ToggleButton 디자인 css -> tailwind 변환및 코드 리팩토링

--- src/components/atomic/ToggleButton.jsx
+++ src/components/atomic/ToggleButton.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const ToggleButton = ({ text, onClick, className, style }) => {
+const ToggleButton = ({ text, onClick, className }) => {
   const [isActive, setIsActive] = useState(false);
 
- 
   const handleClick = () => {
     setIsActive(prevState => !prevState); 
     if (onClick) {
@@ -12,53 +11,24 @@ const ToggleButton = ({ text, onClick, className, style }) => {
     }
   };
 
- 
-  const defaultStyle = {
-    width: 77,
-    height: 35,
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingTop: 9,
-    paddingBottom: 9,
-    display: 'inline-flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    cursor: 'pointer',
-    borderRadius: 50,
-    background: isActive ? '#4D82BE' : 'white',
-    border: isActive ? 'none' : '1px solid #D9D9D9',
-    boxShadow: isActive ? '0px 2px 4px rgba(0, 0, 0, 0.1)' : 'none',
-    ...style 
-  };
-
   return (
     <div
-      className={className}
-      style={defaultStyle}
+      className={`w-[77px] h-[35px] inline-flex items-center justify-center cursor-pointer rounded-full text-sm font-bold whitespace-nowrap ${
+        isActive
+          ? 'bg-blue-500 text-white shadow-dark'
+          : 'bg-white text-blue-500 border border-gray-200'
+      } ${className}`}
       onClick={handleClick}
     >
-      <div
-        style={{
-          color: isActive ? 'white' : '#4D82BE',
-          fontSize: 14,
-          fontFamily: 'Pretendard',
-          fontWeight: '700',
-          lineHeight: '35px', 
-          textAlign: 'center', 
-          whiteSpace: 'nowrap', 
-        }}
-      >
-        {text}
-      </div>
+      {text}
     </div>
   );
 };
 
 ToggleButton.propTypes = {
-  text: PropTypes.string,
+  text: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   className: PropTypes.string,
-  style: PropTypes.object,
 };
 
 export default ToggleButton;
