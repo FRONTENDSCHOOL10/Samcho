a1d3a9f65c176cd1172643f97c763359763d753b
ehh99 <jinung5@gmail.com>
2024-09-03 18:00:57 +0900

    feat: MoodChart 컴포넌트 마크업 - api 관련 환경 수정 및 코드 수정

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,9 +1,17 @@
-import { EmotionRanking, MoodDistributionChart, TopHeader } from '@/components';
+import {
+  EmotionRanking,
+  MoodDistributionChart,
+  TopHeader,
+  MoodChart,
+  YearMonth,
+} from '@/components';
 
 const Chart = () => {
   return (
     <div className="flex flex-col gap-5">
       <TopHeader title="분석보고서" />
+      <YearMonth />
+      <MoodChart />
       <MoodDistributionChart />
       <EmotionRanking />
     </div>
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,10 +1,11 @@
-import { TopHeader } from '@/components';
+import { ToggleTab, TopHeader } from '@/components';
 
 const Post = () => {
   return (
-    <>
+    <div className="flex flex-col gap-5">
       <TopHeader title="우편함" />
-    </>
+      <ToggleTab tab1="알림" tab2="교환일기" />
+    </div>
   );
 };
 
