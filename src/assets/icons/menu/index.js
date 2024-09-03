2f788e732add6d03bf96531bd99d01b6ae795abd
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-03 16:43:32 +0900

    setting: svg 아이콘 추가
    - icon>menu 폴더에 추가
    - path의 fill 속성 current로 변경

--- src/assets/icons/menu/index.js
+++ src/assets/icons/menu/index.js
@@ -2,5 +2,7 @@ import Calendar2 from './calendar2.svg?react';
 import Filter from './filter.svg?react';
 import List from './list.svg?react';
 import Search from './search.svg?react';
+import Delete from './delete.svg?react';
+import Edit from './edit.svg?react';
 
-export { Calendar2, Filter, List, Search };
+export { Calendar2, Filter, List, Search, Delete, Edit };
