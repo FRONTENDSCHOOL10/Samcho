4ee7b30f432931fabaec0dbc10e4a69227043716
ehh99 <jinung5@gmail.com>
2024-09-03 14:27:56 +0900

    feat: 감정 랭킹 더보기 페이지 마크업 - IconRankMoreList 컴포넌트 스타일링 및 코드 구조 수정 - ToggleButton 컴포넌트 스타일링 및 코드 수정 - Footer 컴포넌트 오류 수정 - Home 페이지 스타일링 수정

--- src/components/atomic/IconRankMoreList.jsx
+++ src/components/atomic/IconRankMoreList.jsx
@@ -2,21 +2,22 @@ import PropTypes from 'prop-types';
 
 const IconRankMoreList = ({ rank = 1, emotion = '감정', count = 0 }) => {
   return (
-    <>
-      <div
-        className="inline-box box-content flex w-[149px] h-10 pl-[5px] py-[6px] items-center gap-[15px]"
-        aria-labelledby="rankLabel emotionLabel countLabel"
+    <div
+      className="flex w-full p-2 items-center gap-[15px]"
+      aria-labelledby="rankLabel emotionLabel countLabel"
+    >
+      <span
+        id="rankLabel"
+        className="w-6 text-lg font-semibold text-left text-blue-500"
       >
-        <span id="rankLabel" className="text-lg font-bold text-blue-500">
-          {rank}
-        </span>
-        <span className="inline-block w-10 h-10 rounded-full bg-blue-50"></span>
-        <div className="flex flex-col gap-[5px] items-start content-center text-blue-500 text-sm font-medium">
-          <span id="emotionLabel">{emotion}</span>
-          <span id="countLabel">{`${count}회`}</span>
-        </div>
+        {rank}
+      </span>
+      <span className="inline-block w-10 h-10 rounded-full bg-blue-50"></span>
+      <div className="flex flex-col gap-[5px] items-start content-center text-blue-500 text-sm font-medium">
+        <span id="emotionLabel">{emotion}</span>
+        <span id="countLabel">{`${count}회`}</span>
       </div>
-    </>
+    </div>
   );
 };
 
