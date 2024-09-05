4b1581548b97de7ad26b1a0a7eab37bdf35d0514
paintover <tjdus1139@naver.com>
2024-09-05 17:31:27 +0900

    feat : 단짝관리 페이지 마크업 수정 완료
    - 단짝관리 페이지에서는 단짝 리스트만 보이도록 마크업 수정
    
    resolves #98

--- src/pages/mypage/BuddyManagement.jsx
+++ src/pages/mypage/BuddyManagement.jsx
@@ -2,76 +2,17 @@ import { TopHeader, ToggleTab, PostCard, BuddyCard } from '@/components';
 import { useState } from 'react';
 
 const BuddyManagement = () => {
-  const [activeTab, setActiveTab] = useState('tab1');
-
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-[38px]">
       <TopHeader title="단짝 관리" isShowIcon={true}></TopHeader>
-      <ToggleTab
-        tab1="나의 단짝"
-        tab2="단짝 요청"
-        activeTab={activeTab}
-        onTab1Click={() => setActiveTab('tab1')}
-        onTab2Click={() => setActiveTab('tab2')}
-      />
-      {activeTab === 'tab1' ? (
-        <div
-          className="flex flex-col gap-[15px]"
-          id="panel-buddyList"
-          role="tabpanel"
-          aria-labelledby="tab-buddyList"
-        >
-          <BuddyCard buddyName="두팔" startDate="2023-10-04"></BuddyCard>
-        </div>
-      ) : (
-        <div
-          className="flex flex-col gap-5"
-          id="panel-buddyRequest"
-          role="tabpanel"
-          aria-labelledby="tab-buddyRequest"
-        >
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다. 수락하시겠습니까?"
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-          <PostCard
-            type="bestfriendconfirm"
-            text="두팔님이 단짝 신청을 하였습니다."
-          />
-        </div>
-      )}
+      <div className="flex flex-col gap-5">
+        <BuddyCard buddyName="두팔" startDate="2023-10-04"></BuddyCard>
+        <BuddyCard buddyName="치히로" startDate="2023-09-07"></BuddyCard>
+        <BuddyCard buddyName="소피" startDate="2022-06-16"></BuddyCard>
+        <BuddyCard buddyName="하울" startDate="2024-06-18"></BuddyCard>
+        <BuddyCard buddyName="미츠미" startDate="2023-10-04"></BuddyCard>
+        <BuddyCard buddyName="하쿠" startDate="2023-7-06"></BuddyCard>
+      </div>
     </div>
   );
 };
