a4a78f58e5ce8c5349072b5eb7ac401ed1a8b490
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-05 22:43:49 +0900

    refactor: FeelingCalendar 컴포넌트의 일기 상세로 이동 기능 추가 및 props 수정
    - `isDiaryWritten` props를 제거, mood 데이터 유/무로 아이콘이 있을 때 클릭 시 상세 일기 페이지로 이동하도록 버튼태그로 변경
    - useNavigate 훅을 사용해 클릭 시 해당 날짜의 상세 일기로 이동 기능 추가
    - 추후 DayCircle 아이콘 클릭 시 추후 해당 일자의 일기 작성 페이지로 이동할 수 있도록 처리하기 > 주석 추가 해놓음.

--- src/components/home/FeelingCalendar.jsx
+++ src/components/home/FeelingCalendar.jsx
@@ -1,17 +1,27 @@
 import DayCircle from '@/assets/icons/daycircle/daycircle.svg?react';
 import moods from '@/assets/icons/mood/moods';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
-const FeelingCalendar = ({ day, isDiaryWritten = false, mood }) => {
+const FeelingCalendar = ({ day, mood, date }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/diary/detail', { state: { selectedDate: date } });
+  };
+
   return (
     <div className="flex items-center flex-col gap-[7px]">
-      {isDiaryWritten ? (
-        <img
-          src={moods[mood]}
-          alt={mood}
-          className="w-[44px] h-[44px] cursor-pointer"
-        />
+      {mood ? (
+        <button onClick={handleClick} type="button">
+          <img
+            src={moods[mood]}
+            alt={mood}
+            className="w-[44px] h-[44px] cursor-pointer"
+          />
+        </button>
       ) : (
+        // 추후 일기 해당 날짜 일기 작성으로 이동
         <DayCircle className="fill-blue-50 w-[44px] h-[44px]" />
       )}
       <span>{day}</span>
@@ -21,8 +31,8 @@ const FeelingCalendar = ({ day, isDiaryWritten = false, mood }) => {
 
 FeelingCalendar.propTypes = {
   day: PropTypes.number.isRequired,
-  isDiaryWritten: PropTypes.bool,
   mood: PropTypes.string,
+  date: PropTypes.string.isRequired,
 };
 
 export default FeelingCalendar;
