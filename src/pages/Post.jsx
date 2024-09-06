19cd612f1811f60e5d09fce9784ea89689095114
ehh99 <jinung5@gmail.com>
2024-09-06 18:56:38 +0900

    refactor: 스크롤 관련 웹 접근성 수정 - YearMonth 컴포넌트 max값 수정 - post 페이지 마크업 구조 수정

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,11 +1,10 @@
-import { ToggleTab, TopHeader, PostCard, DiaryCard } from '@/components';
-import { useState } from 'react';
+import { TopHeader, DiaryCard } from '@/components';
 
 const Post = () => {
   return (
-    <div className="flex flex-col gap-10">
+    <section className="flex flex-col gap-5">
       <TopHeader title="교환일기 우편함" />
-      <div className="flex flex-col gap-5">
+      <main className="flex flex-col gap-5">
         <DiaryCard date="2024-08-02" type="date" />
         <DiaryCard date="2024-08-05" type="date" />
         <DiaryCard date="2024-08-11" type="date" />
@@ -13,8 +12,8 @@ const Post = () => {
         <DiaryCard date="2024-08-18" type="date" />
         <DiaryCard date="2024-08-21" type="date" />
         <DiaryCard date="2024-08-30" type="date" />
-      </div>
-    </div>
+      </main>
+    </section>
   );
 };
 
