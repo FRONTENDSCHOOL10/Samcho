8a1fb050d09ad0351b0f3c511474f75d6b7a0e0b
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-10 01:24:59 +0900

    feat: SearchDiaryInput 컴포넌트 마크업/스타일링 완료
    - 일기 검색 인풋 컴포넌트 마크업 / 스타일링 완료
    - 뒤로가기 버튼 클릭 시 메인으로 이동
    - 검색어 입력 시 검색어 초기화 버튼 렌더링

---
+++ src/components/searchdiary/SearchDiaryInput.jsx
@@ -0,0 +1,59 @@
+import { DirectionLeft } from '@/assets/icons/direction';
+import { Close, Search } from '@/assets/icons/menu';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SearchDiaryInput = () => {
+  const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClear = () => {
+    setInputValue('');
+  };
+
+  return (
+    <form className="flex gap-4 pt-5">
+      <button type="button" aria-label="뒤로 가기" onClick={handleBackClick}>
+        <DirectionLeft className=" fill-black" />
+      </button>
+      <div className="flex w-full px-4 py-[10px] border-gray-200 border bg-white rounded-lg items-center ">
+        <label htmlFor="diary-search" className="sr-only">
+          한줄 일기 검색
+        </label>
+        <input
+          id="diary-search"
+          type="text"
+          placeholder="한줄 일기 검색"
+          value={inputValue}
+          onChange={handleChange}
+          className="w-full pr-4 font-semibold outline-none text-gray-450 placeholder:text-gray-200 placeholder:font-normal"
+        />
+        <div className="flex gap-4">
+          {inputValue && (
+            <button
+              type="button"
+              onClick={handleClear}
+              aria-label="검색어 초기화"
+            >
+              <Close className="w-3 h-3 fill-gray-400" />
+            </button>
+          )}
+
+          <button type="button" aria-label="검색 실행">
+            <Search className="w-5 h-5 text-blue" />
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default SearchDiaryInput;
