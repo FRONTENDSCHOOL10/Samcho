e342a908e2b565fe8cc72c87a88c01b1854575c8
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-11 09:32:37 +0900

    refactor: 유틸 함수에서 정렬 코드 제거 및 useFetchAllDiaryData에서 서버 정렬 처리

--- src/utils/groupByMonth.js
+++ src/utils/groupByMonth.js
@@ -1,6 +1,6 @@
 /* 데이터를 연/월 기준으로 그룹화하고 최신순으로 정렬하는 함수 */
 export const groupByMonth = (data) => {
-  const groupedData = data.reduce((group, item) => {
+  return data.reduce((group, item) => {
     const [year, month] = item.date.split('-');
     const dateKey = `${year}년 ${parseInt(month, 10)}월`;
 
@@ -9,18 +9,4 @@ export const groupByMonth = (data) => {
     group[dateKey].push(item);
     return group;
   }, {});
-
-  // 그룹화된 데이터를 최신순으로 정렬하여 반환
-  return Object.keys(groupedData)
-    .sort(
-      (a, b) =>
-        new Date(`${b.replace('년 ', '-').replace('월', '')}-01`) -
-        new Date(`${a.replace('년 ', '-').replace('월', '')}-01`)
-    )
-    .reduce((acc, key) => {
-      acc[key] = groupedData[key].sort(
-        (a, b) => new Date(b.date) - new Date(a.date)
-      );
-      return acc;
-    }, {});
 };
