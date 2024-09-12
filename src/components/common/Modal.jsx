094f73fc9c46b69f98ad6017ba266293b660094f
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-13 07:12:52 +0900

    refactor: Modal 컴포넌트에 닫기 버튼 조건부 렌더링 기능 추가
    - Modal 컴포넌트 showCloseButton  prop을 추가하여 닫기 버튼의 렌더링을 조건부로 처리할 수 있도록 함

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,20 +1,22 @@
-import { memo } from 'react';
 import PropTypes from 'prop-types';
+import { memo } from 'react';
 import { IoClose } from 'react-icons/io5';
 
-const Modal = ({ isOpen, closeModal, children }) => {
+const Modal = ({ isOpen, closeModal, children, showCloseButton = true }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 z-[1000] backdrop-blur-[1px]">
       <div className="relative w-full max-w-[400px] p-4 bg-white rounded-lg shadow-lg">
-        <button
-          onClick={closeModal}
-          type="button"
-          className="absolute top-[18px] right-5"
-        >
-          <IoClose aria-hidden={true} className="w-6 h-6" color="#393939" />
-        </button>
+        {showCloseButton && (
+          <button
+            onClick={closeModal}
+            type="button"
+            className="absolute top-[18px] right-5"
+          >
+            <IoClose aria-hidden={true} className="w-6 h-6" color="#393939" />
+          </button>
+        )}
         {children}
       </div>
     </div>
@@ -25,6 +27,7 @@ Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   closeModal: PropTypes.func.isRequired,
   children: PropTypes.node,
+  showCloseButton: PropTypes.bool,
 };
 
 export default memo(Modal);
