2828042c99461a14318e1ba245855396bb7504d0
paintover <tjdus1139@naver.com>
2024-09-14 01:21:17 +0900

    modify : buddyCard 1일 전은 n시간으로 표시될 수 있도록 수정
    - buddyCard에서 시간차이도 계산하여, 24시간 미만은 n시간 쨰 단짝중으로 표시될 수 있도록 수정
    
    resolves #231

--- src/components/buddy/BuddyCard.jsx
+++ src/components/buddy/BuddyCard.jsx
@@ -1,14 +1,13 @@
 import { PropTypes } from 'prop-types';
-import { differenceInDays, format } from 'date-fns';
+import { differenceInDays, differenceInHours } from 'date-fns';
 import { memo } from 'react';
 import pb from '@/api/pb';
 import toast from 'react-hot-toast';
 
 const BuddyCard = ({ buddyName, startDate, buddyId, onDelete }) => {
-  const daysDifference = differenceInDays(
-    format(new Date(), 'yyyy-MM-dd'),
-    startDate
-  );
+  const hoursDifference = differenceInHours(new Date(), new Date(startDate));
+
+  const daysDifference = differenceInDays(new Date(), new Date(startDate));
 
   const handleDelete = async () => {
     try {
@@ -30,7 +29,11 @@ const BuddyCard = ({ buddyName, startDate, buddyId, onDelete }) => {
     <div className="flex justify-between bg-white py-[13px] px-[18px] rounded-[14px] shadow-light items-center">
       <p className="text-lg font-medium">{buddyName}</p>
       <div className="flex flex-row gap-[15px] items-center">
-        <p className="font-medium text-gray-300">{`${daysDifference}일 째 단짝 중`}</p>
+        <p className="font-medium text-gray-300">
+          {hoursDifference < 24
+            ? `${hoursDifference}시간 째 단짝 중`
+            : `${daysDifference}일 째 단짝 중`}
+        </p>
         <button
           type="button"
           aria-label={`${buddyName}님과 절교 `}
--- src/pages/mypage/BuddyManagement.jsx
+++ src/pages/mypage/BuddyManagement.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { BuddyCard, TopHeader } from '@/components';
 import { useFetchAllBuddyData } from '@/hooks';
-import { format } from 'date-fns';
+import { formatDate } from 'date-fns';
 
 const BuddyManagement = () => {
   const { buddyData, loading } = useFetchAllBuddyData();
@@ -36,7 +36,7 @@ const BuddyManagement = () => {
           <BuddyCard
             key={buddyId}
             buddyName={buddyName}
-            startDate={format(new Date(created), 'yyyy-MM-dd')}
+            startDate={formatDate(created, 'yyyy-MM-dd HH:mm:ss')}
             buddyId={buddyId}
             onDelete={handleDelete}
           />
