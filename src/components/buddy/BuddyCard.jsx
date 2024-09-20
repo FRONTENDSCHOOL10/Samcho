cbb0deeee67a02012c499ef80d46b71b23cc1fbf
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-09-20 21:32:19 +0900

    bugfix: 절교 처리시 오류 현상 해결 및 toast.promise 적용
    -이전 로직에서 교환일기 알림이 없을 경우 404 에러가 발생하였음.
    - 이를 처리하기 위해 404 에러는 교환일기 알림이 없는 정상적인 상황으로 간주하고, 다른 에러만 처리하도록 수정

--- src/components/buddy/BuddyCard.jsx
+++ src/components/buddy/BuddyCard.jsx
@@ -1,12 +1,12 @@
 import pb from '@/api/pb';
 import { useModal } from '@/hooks';
+import { useNotificationStore } from '@/stores';
+import { authUtils } from '@/utils';
 import { differenceInDays, differenceInHours } from 'date-fns';
 import { PropTypes } from 'prop-types';
 import { memo } from 'react';
 import toast from 'react-hot-toast';
 import { ConfirmModal } from '..';
-import { authUtils } from '@/utils';
-import { useNotificationStore } from '@/stores';
 
 const BuddyCard = ({ buddyName, startDate, buddyId, onDelete }) => {
   const hoursDifference = differenceInHours(new Date(), new Date(startDate));
@@ -18,71 +18,96 @@ const BuddyCard = ({ buddyName, startDate, buddyId, onDelete }) => {
   const { isOpen, openModal, closeModal } = useModal();
 
   const handleDelete = async () => {
-    try {
-      const { user } = authUtils.getAuth();
-      const userId = user.id;
-
-      if (!user) {
-        toast.error(
-          '사용자 인증정보를 불러오지 못했어요. 다시 시도해주세요 😥'
-        );
-        return;
-      }
-
-      const record = await pb
-        .collection('buddy')
-        .getFirstListItem(
-          `recipient = "${buddyId}" && requester = "${userId}" || recipient = "${userId}" && requester = "${buddyId}"`
-        );
-
-      if (!record) {
-        toast.error('단짝 정보를 찾을 수 없어요. 다시 시도해주세요 😥');
-        return;
-      }
-
-      await pb.collection('buddy').delete(record.id);
-
-      // 교환일기 알림 삭제
-      const notification = await pb
-        .collection('notification')
-        .getFirstListItem(
-          `(recipient = "${userId}" && requester = "${buddyId}" && type = "교환일기") || (recipient = "${buddyId}" && requester = "${userId}" && type = "교환일기")`
-        );
-
-      if (notification) {
-        await pb.collection('notification').delete(notification.id);
-      }
-
-      // 교환일기 삭제
-      const posts = await pb.collection('post').getFullList({
-        filter: `(recipient = "${buddyId}" && requester = "${userId}") || (recipient = "${userId}" && requester = "${buddyId}")`,
-      });
-
-      if (posts.length > 0) {
-        for (const post of posts) {
-          await pb.collection('post').delete(post.id);
-        }
-      }
-
-      // 알림 상태 업데이트
-      const updatedNotifications = await pb
-        .collection('notification')
-        .getFullList({
-          filter: `recipient = "${userId}"`,
-        });
-      setNotifications(
-        updatedNotifications.map((list) => ({
-          ...list,
-          read: false,
-        }))
-      );
-
-      toast.success('단짝을 멀리 보냈습니다.');
-      closeModal('breachModal');
-      onDelete(buddyId);
-    } catch (error) {
-      console.error('단짝 삭제 중 오류 발생:', error);
+    const auth = authUtils.getAuth();
+    if (!auth || !auth.user) {
+      toast.error('사용자 인증정보를 불러오지 못했어요. 다시 시도해주세요 😥');
+      return;
     }
+
+    const userId = auth.user.id;
+
+    toast.promise(
+      (async () => {
+        try {
+          // 단짝 삭제
+          const record = await pb
+            .collection('buddy')
+            .getFirstListItem(
+              `recipient = "${buddyId}" && requester = "${userId}" || recipient = "${userId}" && requester = "${buddyId}"`
+            );
+
+          if (!record) {
+            throw new Error('단짝 정보를 찾을 수 없어요. 다시 시도해주세요 😥');
+          }
+
+          await pb.collection('buddy').delete(record.id);
+
+          // 교환일기 알림 삭제
+          try {
+            const notification = await pb
+              .collection('notification')
+              .getFirstListItem(
+                `(recipient = "${userId}" && requester = "${buddyId}" && type = "교환일기") || (recipient = "${buddyId}" && requester = "${userId}" && type = "교환일기")`
+              );
+
+            if (notification) {
+              await pb.collection('notification').delete(notification.id);
+            }
+          } catch (error) {
+            if (error.status !== 404) {
+              console.error('교환일기 알림 삭제 중 오류 발생', error);
+              throw error;
+            }
+          }
+
+          // 교환일기 삭제
+          try {
+            const posts = await pb.collection('post').getFullList({
+              filter: `(recipient = "${buddyId}" && requester = "${userId}") || (recipient = "${userId}" && requester = "${buddyId}")`,
+            });
+
+            if (posts.length > 0) {
+              for (const post of posts) {
+                await pb.collection('post').delete(post.id);
+              }
+            }
+          } catch (error) {
+            console.error('교환일기 삭제 중 오류 발생', error);
+            throw new Error(
+              '교환일기 삭제 중 문제가 발생했어요. 다시 시도해주세요 😥'
+            );
+          }
+
+          // 알림 상태 업데이트
+          try {
+            const updatedNotifications = await pb
+              .collection('notification')
+              .getFullList({
+                filter: `recipient = "${userId}"`,
+              });
+            setNotifications(
+              updatedNotifications.map((list) => ({
+                ...list,
+                read: false,
+              }))
+            );
+          } catch (error) {
+            console.error('알림 상태 업데이트 중 오류 발생', error);
+          }
+
+          closeModal('breachModal');
+          onDelete(buddyId);
+        } catch (error) {
+          console.error('단짝 삭제 중 오류 발생:', error);
+          throw error;
+        }
+      })(),
+      {
+        loading: '단짝을 삭제하는 중입니다...⏳',
+        success: '단짝을 멀리 보냈습니다 👋',
+        error: '단짝 삭제 중 오류가 발생했어요. 다시 시도해주세요 😥',
+      }
+    );
   };
 
   return (
